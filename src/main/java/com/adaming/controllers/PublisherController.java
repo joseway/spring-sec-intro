e2807cc96f129afaaa0011141d9ed46c0bbe83b2
noella <noellanzeba@gmail.com>
2020-01-31 08:52:34 +0000

    security extras. limit access to pages

--- src/main/java/com/adaming/controllers/PublisherController.java
+++ src/main/java/com/adaming/controllers/PublisherController.java
@@ -1,5 +1,6 @@
 package com.adaming.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value="/publishers")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class PublisherController {
 
 	@GetMapping(value="")
